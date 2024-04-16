b29f2ccd23a797cf8576ed2a5879c631e16321e8
arromero4 <arromero0408@gmail.com>
2024-04-15 22:10:34 -0600

    Added: Learning Flutter Bloc

---
+++ flutter_bloc_docs/README.md
@@ -0,0 +1,3 @@
+# flutter_bloc_docs
+
+A new Flutter project.
---
+++ flutter_bloc_docs/lib/main.dart
@@ -0,0 +1,54 @@
+//import 'package:flutter/material.dart';
+//import 'package:flutter_bloc/flutter_bloc.dart';
+//import 'package:flutter_bloc_docs/counter_cubit.dart';
+//import 'package:flutter_bloc_docs/simple_bloc_observer.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_docs/counter_bloc.dart';
+import 'package:flutter_bloc_docs/simple_bloc_observer.dart';
+
+// Stream<int> countStream(int max) async* {
+//   for (int i = 0; i < max; i++) {
+//     yield i;
+//   }
+// }
+
+// Future<int> sumStream(Stream<int> stream) async {
+//   int sum = 0;
+//   await for (int value in stream) {
+//     sum += value;
+//   }
+//   return sum;
+// }
+
+Future<void> main() async {
+  //runApp(const MainApp());
+  Bloc.observer = SimpleBlocObserver();
+  CounterBloc()
+    ..add(CounterIncrementPressed())
+    ..close();
+  // final bloc = CounterBloc();
+  // final suscription = bloc.stream.listen(print);
+  // bloc.add(CounterIncrementPressed());
+  // await Future.delayed(Duration.zero);
+  // await suscription.cancel();
+  // await bloc.close();
+
+  // Bloc.observer = SimpleBlocObserver();
+  // CounterCubit()
+  //   ..increment()
+  //   ..close();
+
+  // final cubit = CounterCubit();
+  // final subscription = cubit.stream.listen(print);
+  // cubit.increment();
+  // await Future.delayed(Duration.zero);
+  // await subscription.cancel();
+  // cubit.close();
+  //initialize a stream of integers 0-9
+  //Stream<int> stream = countStream(10);
+  //Compute the sum of the stream of integers
+  //int sum = await sumStream(stream);
+  //print the sum
+  //print(sum);
+}
