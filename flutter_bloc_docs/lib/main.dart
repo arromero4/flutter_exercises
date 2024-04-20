4fb4e29dfb1dcc82931fbe2c7fdf12b3216608c0
arromero4 <arromero0408@gmail.com>
2024-04-20 03:46:10 -0600

    Added: Learning Flutter Bloc

--- flutter_bloc_docs/lib/main.dart
+++ flutter_bloc_docs/lib/main.dart
@@ -1,12 +1,25 @@
 //import 'package:flutter/material.dart';
-//import 'package:flutter_bloc/flutter_bloc.dart';
-//import 'package:flutter_bloc_docs/counter_cubit.dart';
-//import 'package:flutter_bloc_docs/simple_bloc_observer.dart';
-
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_docs/counter_bloc.dart';
+import 'package:flutter_bloc_docs/counter_page.dart';
+//import 'package:flutter_bloc_docs/counter_cubit.dart';
 import 'package:flutter_bloc_docs/simple_bloc_observer.dart';
+import 'package:flutter_bloc_docs/counter_bloc.dart';
 
+void main() => runApp(CounterApp());
+
+class CounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocProvider(
+        create: (_) => CounterBloc(),
+        child: CounterPage(),
+      ),
+    );
+  }
+}
 // Stream<int> countStream(int max) async* {
 //   for (int i = 0; i < max; i++) {
 //     yield i;
@@ -21,12 +34,12 @@ import 'package:flutter_bloc_docs/simple_bloc_observer.dart';
 //   return sum;
 // }
 
-Future<void> main() async {
-  //runApp(const MainApp());
-  Bloc.observer = SimpleBlocObserver();
-  CounterBloc()
-    ..add(CounterIncrementPressed())
-    ..close();
+// Future<void> main() async {
+//   //runApp(const MainApp());
+//   Bloc.observer = SimpleBlocObserver();
+//   CounterBloc()
+//     ..add(CounterIncrementPressed())
+//     ..close();
   // final bloc = CounterBloc();
   // final suscription = bloc.stream.listen(print);
   // bloc.add(CounterIncrementPressed());
@@ -51,4 +64,4 @@ Future<void> main() async {
   //int sum = await sumStream(stream);
   //print the sum
   //print(sum);
-}
+//}
